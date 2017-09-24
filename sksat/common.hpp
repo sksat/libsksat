1e47d4963883046ade290fb0477a73cd1997c449
sksat <sksat.tec@gmail.com>
2017-09-24 18:16:09 +0900

    [UPDATE]

--- sksat/common.hpp
+++ sksat/common.hpp
@@ -7,8 +7,42 @@
 
 namespace sksat
 {
+	using ::FILE;
+	using ::fpos_t;
+
+	using ::clearerr;
+	using ::fclose;
+	using ::feof;
+	using ::ferror;
+	using ::fflush;
+	using ::fgetc;
+	using ::fgetpos;
+	using ::fgets;
+	using ::fopen;
+	using ::fprintf;
+	using ::fputc;
+	using ::fputs;
+	using ::fread;
+	using ::freopen;
+	using ::fscanf;
+	using ::fseek;
+	using ::fsetpos;
+	using ::ftell;
+	using ::fwrite;
+	using ::getc;
+	using ::getchar;
+
 	using ::popen;
 	using ::pclose;
 }
+
+#ifndef NO_STL
+	#include <vector>
+	#include <string>
+	namespace sksat {
+		using std::vector;
+		using std::string;
+	}
+#endif
 
 #endif
--- sksat/math/gnuplot.hpp
+++ sksat/math/gnuplot.hpp
@@ -1,4 +1,3 @@
-#include <string>
 #include <sksat/common.hpp>
 
 namespace sksat {
@@ -9,25 +8,25 @@ public:
 	enum MODE {DEFAULT, PERSIST, };
 	gnuplot() : gnuplot(DEFAULT) {}
 	gnuplot(MODE m) : mode(m) {
-		std::string arg("gnuplot");
+		sksat::string arg("gnuplot");
 		if(mode == PERSIST) arg += " -persist";
 		gp = sksat::popen(arg.c_str(), "w");
 	}
 	~gnuplot(){ pclose(gp); }
 
 	void command(const char *com){
-		std::fprintf(gp, "%s\n", com);
+		sksat::fprintf(gp, "%s\n", com);
 	}
-	void command(std::string &com){
+	void command(sksat::string &com){
 		command(com.c_str());
 	}
 	void set(const char *arg){
-		std::fprintf(gp, "set %s\n", arg);
+		sksat::fprintf(gp, "set %s\n", arg);
 	}
 	void set_grid()	{ set("grid"); }
 	void flush()	{ fflush(gp); } 
 	void plot(const char *arg){
-		std::fprintf(gp, "plot %s\n", arg);
+		sksat::fprintf(gp, "plot %s\n", arg);
 	}
 	void plot()		{ plot(""); }
 	void quit()		{ command("quit"); }
