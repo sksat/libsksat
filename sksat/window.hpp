2c491c1bd14a4ffcf18a51a3f297e9fbd3d14d7f
sksat <sksat.tec@gmail.com>
2017-10-06 19:19:49 +0900

    [UPDATE]

--- sksat/window.hpp
+++ sksat/window.hpp
@@ -8,12 +8,12 @@ namespace sksat {
 
 class window_base {
 public:
-	window_base() : xsize(default_xsize), ysize(default_ysize) {}
+	window_base() : opend(false), xsize(default_xsize), ysize(default_ysize) {}
 
-	virtual void open(size_t x, size_t y) = 0;
-	void open(sksat::string &t){ set_title(t); open(xsize, ysize); }
+	virtual void open() = 0;
+	void open(size_t x, size_t y){ open(); set_size(x,y); }
+	void open(sksat::string &t){ set_title(t); open(); }
 	void open(sksat::string &t, size_t x, size_t y){ set_title(t); open(x,y); }
-	void open(){ open(xsize, ysize); }
 
 	virtual void close() = 0;
 
@@ -22,11 +22,16 @@ public:
 	void set_title(sksat::string &t){ title = t; update_title(t); }
 	sksat::string get_title(){ return title; }
 
-	virtual void set_size(size_t x, size_t y){ xsize=x; ysize=y; update_size(x,y); }
+	virtual void set_size(size_t x, size_t y){
+		xsize=x;
+		ysize=y;
+		if(opend)
+			update_size(x,y);
+	}
 	void set_xsize(size_t x){ set_size(x, ysize); }
 	void set_ysize(size_t y){ set_size(xsize, y); }
-	size_t get_xsize(){ return xsize; }
-	size_t get_ysize(){ return ysize; }
+	size_t get_xsize() const { return xsize; }
+	size_t get_ysize() const { return ysize; }
 protected:
 	virtual void update_title(sksat::string &t) = 0;
 	virtual void update_size(size_t x, size_t y) = 0;
@@ -34,6 +39,7 @@ public:
 	static size_t default_xsize, default_ysize;
 	static size_t default_xpos, default_ypos;
 protected:
+	bool opend;
 	size_t xsize, ysize;
 	size_t xpos, ypos;
 	sksat::string title;
