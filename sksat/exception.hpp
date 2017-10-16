0160a742cd4b65465aeb37d1cffbc230bd4bf1fd
sksat <sksat.tec@gmail.com>
2017-10-16 20:03:37 +0900

    [ADD] exception class

---
+++ sksat/exception.hpp
@@ -0,0 +1,13 @@
+#ifndef SKSAT_EXCEPTION_HPP_
+#define SKSAT_EXCEPTION_HPP_
+
+#include <sksat/common.hpp>
+#include <exception>
+
+namespace sksat {
+
+class exception : public std::exception {  };
+
+} // namespace sksat
+
+#endif
